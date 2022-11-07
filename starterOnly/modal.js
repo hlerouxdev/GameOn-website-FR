6e62adbb8f4692a902363a84695efc85e3071534
Hugo Leroux <hugolerouxmusic@gmail.com>
2022-11-07 04:16:48 +0100

    error handling streamlining

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -30,25 +30,23 @@ function closeModal() {
 };
 
 // regex list 
-const regexlist = {
+const regexList = {
   name: /^[a-zA-Z ]+$/,
   email: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-  date: /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/
+  date: /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/,
+  number: /^[1-9][0-9]?$|^100$/
 };
 
-// error class
-
-class Inputerror{
-  constructor(_domElem, _errorMessage) {
-    this.domElem = _domElem;
-    this.errorMessage = _errorMessage;
-  }
-  createError() {
-    console.log("test class method");
-    this.domElem.setAttribute("data-error", this.errorMessage)
-    this.domElem.setAttribute("data-error-visible", "true")
-  }
-};
+// error handling
+function createError(domElem, regex, inputName) {
+  let errorMessage
+  if(!domElem.value) errorMessage = "ce champs ne peut pas être vide"
+  if(!regex.test(domElem.value)) errorMessage = `veuillez entrer un ${inputName}`
+  if(regex.test(domElem.value) && domElem.value) return true
+  domElem.setAttribute("data-error", errorMessage)
+  domElem.setAttribute("data-error-visible", "true")
+  return false
+}
 
 function removeError(domElem) {
   domElem.removeAttribute("data-error")
@@ -59,58 +57,19 @@ function removeError(domElem) {
 function checkInputs(){
   let valid = true;
   const inputsArray = [...formData];
+  //indivudual input declaration
+  const formFirstName = inputsArray[0];
+  const formLastName = inputsArray[1];
+  const formEmail = inputsArray[2]
+  const formDate = inputsArray[3];
+  const formNumber = inputsArray[4]
 
-  // Checks each field individually for regex conmformity
-  if(!regexlist.name.test(inputsArray[0].querySelector(".text-control").value)) {
-    const newError = new Inputerror(inputsArray[0], "Veuillez entrer un prénom valide")
-    newError.createError()
-    valid = false;
-  }
-  else {
-    removeError(inputsArray[0])
-  }
-  if(!regexlist.name.test(inputsArray[1].querySelector(".text-control").value)) {
-    const newError = new Inputerror(inputsArray[1], "Veuillez entrer un nom valide")
-    newError.createError()
-    valid = false;
-  }
-  else {
-    removeError(inputsArray[2])
-  }
-  if(!regexlist.email.test(inputsArray[2].querySelector(".text-control").value)) {
-    const newError = new Inputerror(inputsArray[2], "Veuillez entrer un email valide")
-    newError.createError()
-    valid = false;
-  }
-  else {
-    removeError(inputsArray[2])
-  }
-  if(!regexlist.date.test(inputsArray[3].querySelector(".text-control").value)) {
-    const newError = new Inputerror(inputsArray[3], "Veuillez entrer une date valide")
-    newError.createError()
-    valid = false;
-  } else {
-    removeError(inputsArray[3])
-  }
-  if(inputsArray[4].querySelector(".text-control").value === NaN ||
-  0 > inputsArray[4].querySelector(".text-control").value > 100) {
-    const newError = new Inputerror(inputsArray[4], "Veuillez entrer un nombre")
-    newError.createError()
-    valid = false;
-  } else {
-    removeError(inputsArray[4])
-  }
-
-  // loop checking for empty fields
-  for (let i=0; i < 5 ; i++) {
-    const value = inputsArray[i].querySelector(".text-control").value;
-    if(!value) {
-      console.log("empty");
-      const newError = new Inputerror(inputsArray[i], "Ce champs ne peut être vide")
-      newError.createError()
-      valid = false;
-    }
-  }
+  // Checks each field for regex validation
+  if(!createError(formFirstName, regexList.name, "prénom valide")) valid = false
+  if(createError(!formLastName, regexList.name, "nom valide")) valid =false
+  if(!createError(formEmail, regexList.email, "email valide")) valid = false
+  if(!createError(formDate, regexList.date, "date valide")) valid = false
+  if(!createError(formNumber, regexList.number, "nombre entre 0 et 100")) valid = false
 
   return valid;
 };
